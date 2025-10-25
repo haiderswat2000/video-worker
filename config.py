0ad9b2998db0a2a650c9c4b453ca8acff2efb10e
haiderswat2000 <swatiraq4444@gmail.com>
2025-10-25 16:42:52 +0300

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-APP_NAME        = "video-worker"
-DEBUG           = True
-HOST            = "0.0.0.0"
-PORT            = 10000          # Render سيستبدله تلقائياً بـ $PORT
-MAX_CONCURRENCY = 2
-STORAGE_DIR     = Path("./storage").absolute()
-STORAGE_DIR.mkdir(parents=True, exist_ok=True)
-CLEANUP_AFTER_DONE = 3600        # ثواني
